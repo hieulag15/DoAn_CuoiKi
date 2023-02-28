633eb012b5c07d680e012b8e0622b56ecd213846
hieulag15 <hieuthanhtran12@gmail.com>
2023-02-28 13:42:55 +0700

    them

---
+++ WinFormsApp1/FDangNhap.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WinFormsApp1
+{
+    partial class FDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.errorProviderUser = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderPass = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.Location = new System.Drawing.Point(377, 354);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(142, 40);
+            this.btnLogin.TabIndex = 14;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblWin
+            // 
+            this.lblWin.AutoSize = true;
+            this.lblWin.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblWin.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lblWin.Location = new System.Drawing.Point(188, 64);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(316, 55);
+            this.lblWin.TabIndex = 13;
+            this.lblWin.Text = "NGÂN HÀNG";
+            // 
+            // txtPass
+            // 
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPass.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPass.Location = new System.Drawing.Point(243, 265);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(394, 43);
+            this.txtPass.TabIndex = 12;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUser.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUser.Location = new System.Drawing.Point(243, 180);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(394, 43);
+            this.txtUser.TabIndex = 11;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblPass.Location = new System.Drawing.Point(27, 262);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(155, 36);
+            this.lblPass.TabIndex = 10;
+            this.lblPass.Text = "Passwrord:";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUser.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.lblUser.Location = new System.Drawing.Point(27, 174);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(164, 36);
+            this.lblUser.TabIndex = 9;
+            this.lblUser.Text = "User Name:";
+            // 
+            // errorProviderUser
+            // 
+            this.errorProviderUser.BlinkRate = 300;
+            this.errorProviderUser.ContainerControl = this;
+            // 
+            // errorProviderPass
+            // 
+            this.errorProviderPass.BlinkRate = 300;
+            this.errorProviderPass.ContainerControl = this;
+            // 
+            // FDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 434);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblWin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUser);
+            this.Name = "FDangNhap";
+            this.Text = "ĐĂNG NHẬP";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private Label lblWin;
+        private TextBox txtPass;
+        private TextBox txtUser;
+        private Label lblPass;
+        private Label lblUser;
+        private ErrorProvider errorProviderUser;
+        private ErrorProvider errorProviderPass;
+    }
+}
---
+++ WinFormsApp1/FDangNhap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class FDangNhap : Form
+    {
+        public FDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Login successful!", "Infomation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Reset();
+        }
+
+        private void Reset()
+        {
+            txtUser.Text = "";
+            txtPass.Text = "";
+        }
+    }
+}
