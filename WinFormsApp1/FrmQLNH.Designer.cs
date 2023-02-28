90db9c360924211f8b888e9add53e56a2cb7eeb5
BaoNun1205 <baole.120503@gmail.com>
2023-02-28 11:41:40 +0700

    gd

--- WinFormsApp1/FrmQLNH.Designer.cs
+++ WinFormsApp1/FrmQLNH.Designer.cs
@@ -38,12 +38,9 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -136,7 +133,7 @@
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.Salmon;
+            this.panel2.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(1164, 0);
             this.panel2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
@@ -157,36 +154,14 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::WinFormsApp1.Properties.Resources.digital_banking_platform_making_1;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 145);
+            this.pictureBox1.Image = global::WinFormsApp1.Properties.Resources.chuyen_doi_so_ngan_hang_3;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1157, 800);
+            this.pictureBox1.Size = new System.Drawing.Size(1157, 945);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.CadetBlue;
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1157, 139);
-            this.panel3.TabIndex = 8;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(251, 41);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(600, 51);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Mang niềm vui đến cho mọi người";
             // 
             // FrmQLNH
             // 
@@ -194,7 +169,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
             this.ClientSize = new System.Drawing.Size(1707, 945);
-            this.Controls.Add(this.panel3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -210,8 +184,6 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -228,7 +200,5 @@
         private Panel panel2;
         private Label label1;
         private PictureBox pictureBox1;
-        private Panel panel3;
-        private Label label2;
     }
 }
--- WinFormsApp1/FrmQLNH.cs
+++ WinFormsApp1/FrmQLNH.cs
@@ -31,10 +31,5 @@ namespace WinFormsApp1
         {
 
         }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
